fb5c388c99baa6ce437e28022f6f91f8652aa043
LwinMeMeKhaing.github.io <www.lwinmeme@gmail.com>
2018-06-11 10:38:20 +0630

    add ex13.py

--- ex13.py
+++ ex13.py
@@ -1,9 +1,8 @@
 from sys import argv
-script=argv,
-first=argv,
-second=argv,
-third=argv
-print("The script is called :",script)
-print("Your first variable is:",first)
-print("Your second variable is:",second)
-print("Your third variable is:",third)
+#read the WYSS section for how to run this
+script ,first ,second ,third=argv
+
+print("The script is called:",script)
+print("Your first variable is :",first)
+print("Your second variable is :",second)
+print("Your third variable is :",third)
